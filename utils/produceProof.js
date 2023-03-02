24d1dbd299d2811fedd8cc12afafa7c016c91d13
ricearoni68 <sclement2@gmail.com>
2023-03-02 13:14:02 -0800

    Update input JSON file and JS to produce the file

--- utils/produceProof.js
+++ utils/produceProof.js
@@ -1,14 +1,15 @@
-// Utility to create input for MerkleTree Gift
-// node utils\produceProof.js
+// Utility to create input for MerkleTree Gift.
+// Name selected from the niceList is random at this point in time
+// Usage: node utils/produceProof.js
 
 const fs = require("fs");
-const niceList = require("./niceList.json"); // Used to generate cases with no name provided
+const niceList = require("./niceList.json"); // Used to get a valid name
 const MerkleTree = require("./MerkleTree"); // Used to generate proof
 const leaves = niceList;
 const niceListTree = new MerkleTree(leaves);
 const name = niceList[Math.floor(Math.random() * 1000)]; // pick a random name from the Nice List
 const index = niceList.findIndex((n) => n === name);
-proof = niceListTree.getProof(index);
+proof = niceListTree.getProof(index); // valid proof for the selected name
 
 try {
   let out = '{"name": "' + name + '",' + '\n "proof": ';
